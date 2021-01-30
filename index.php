3be4f8a9c2de6ce83af616320c448db3fb3705a5
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-01-29 21:17:56 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+!DOCTYPE html>
+<html lang="en-US">
+<!--
+Christopher Burgess
+1/29/2021
+index.php
+-->
+<head>
+	<title>Webserver</title>
+	<meta charset="utf-8"/>
+	<!-- <link href="style.css" rel="css/stylesheet"/> -->
+</head>
+<body>
+	<nav>
+	</nav>
+	<main>
+	Hello World
+	</main>
+	<footer>
+	</footer>
+</body>
+</html>
