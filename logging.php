c116cda6ab26ae21dc3ef44b9437c4555a0b2408
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-02-01 23:09:41 -0500

    Add files via upload

--- logging.php
+++ logging.php
@@ -6,12 +6,18 @@ try {
 	$query = 'INSERT INTO auditLog (Event, Severity, IP, Time, Username, Password, SecQuestion, SecAnswer) VALUES (:event, :severity, :ip, NOW(), :user, :pass, :question, :answer);';
 	$dbquery = $myDBconnection -> prepare($query);
 	$dbquery -> bindValue(':event', $event);
+	echo $event;
 	$dbquery -> bindValue(':severity', $severity);
+	echo $severity;
 	$dbquery -> bindValue(':ip', $IP);
 	$dbquery -> bindValue(':user', $username); 
+	echo $username;
 	$dbquery -> bindValue(':pass', $password);
+	echo $password;
 	$dbquery -> bindValue(':question', $secquestion);
+	echo $secquestion;
 	$dbquery -> bindValue(':answer', $secanswer);
+	echo $secanswer;
 	$dbquery -> execute();
 } catch (PDOException $e) {
 	$error_message = $e->getMessage();
