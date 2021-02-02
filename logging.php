93e10c1e0e572e00a5912d6c74c12d965c22f9f5
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-02-01 23:14:14 -0500

    Add files via upload

--- logging.php
+++ logging.php
@@ -1,24 +1,24 @@
 <?php 
 function auditlog($event, $severity, $username, $password, $secquestion, $secanswer) {
 $IP = $_SERVER['REMOTE_ADDR'];
-echo IP;
+echo $IP;
+echo $event;
+echo $severity;
+echo $username;
+echo $password;
+echo $secquestion;
+echo $secanswer;
 try {
 	//Username, Password, Security Question, and Security Answer can be NULL in case they aren't used in certain events or are left blank
 	$query = 'INSERT INTO auditLog (Event, Severity, IP, Time, Username, Password, SecQuestion, SecAnswer) VALUES (:event, :severity, :ip, NOW(), :user, :pass, :question, :answer);';
 	$dbquery = $myDBconnection -> prepare($query);
 	$dbquery -> bindValue(':event', $event);
-	echo $event;
 	$dbquery -> bindValue(':severity', $severity);
-	echo $severity;
 	$dbquery -> bindValue(':ip', $IP);
 	$dbquery -> bindValue(':user', $username); 
-	echo $username;
 	$dbquery -> bindValue(':pass', $password);
-	echo $password;
 	$dbquery -> bindValue(':question', $secquestion);
-	echo $secquestion;
 	$dbquery -> bindValue(':answer', $secanswer);
-	echo $secanswer;
 	$dbquery -> execute();
 } catch (PDOException $e) {
 	$error_message = $e->getMessage();
