b68caa715045f2573f63c8b7d40ceff3ef9608fd
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-02-01 23:06:19 -0500

    Add files via upload

--- logging.php
+++ logging.php
@@ -1,7 +1,6 @@
 <?php 
 function auditlog($event, $severity, $username, $password, $secquestion, $secanswer) {
 $IP = $_SERVER['REMOTE_ADDR'];
-echo $IP;
 try {
 	//Username, Password, Security Question, and Security Answer can be NULL in case they aren't used in certain events or are left blank
 	$query = 'INSERT INTO auditLog (Event, Severity, IP, Time, Username, Password, SecQuestion, SecAnswer) VALUES (:event, :severity, :ip, NOW(), :user, :pass, :question, :answer);';
@@ -9,10 +8,10 @@ try {
 	$dbquery -> bindValue(':event', $event);
 	$dbquery -> bindValue(':severity', $severity);
 	$dbquery -> bindValue(':ip', $IP);
-	$dbquery -> bindValue(':user', $user); 
-	$dbquery -> bindValue(':pass', $pass);
-	$dbquery -> bindValue(':question', $question);
-	$dbquery -> bindValue(':answer', $answer);
+	$dbquery -> bindValue(':user', $username); 
+	$dbquery -> bindValue(':pass', $password);
+	$dbquery -> bindValue(':question', $secquestion);
+	$dbquery -> bindValue(':answer', $secanswer);
 	$dbquery -> execute();
 } catch (PDOException $e) {
 	$error_message = $e->getMessage();
