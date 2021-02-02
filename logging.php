71b1ad348be3b9c066507f1781068b9a90dfd59f
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-02-01 23:51:52 -0500

    Add files via upload

--- logging.php
+++ logging.php
@@ -1,16 +1,21 @@
 <?php 
 function auditlog($event, $severity, $username, $password, $secquestion, $secanswer) {
 $IP = $_SERVER['REMOTE_ADDR'];
-//Username, Password, Security Question, and Security Answer can be NULL in case they aren't used in certain events or are left blank
-$query = 'INSERT INTO auditLog (Event, Severity, IP, Time, Username, Password, SecQuestion, SecAnswer) VALUES (:event, :severity, :ip, NOW(), :user, :pass, :question, :answer);';
-$dbquery = $myDBconnection -> prepare($query);
-$dbquery -> bindValue(':event', $event);
-$dbquery -> bindValue(':severity', $severity);
-$dbquery -> bindValue(':ip', $IP);
-$dbquery -> bindValue(':user', $username); 
-$dbquery -> bindValue(':pass', $password);
-$dbquery -> bindValue(':question', $secquestion);
-$dbquery -> bindValue(':answer', $secanswer);
-$dbquery -> execute();
+try {
+	//Username, Password, Security Question, and Security Answer can be NULL in case they aren't used in certain events or are left blank
+	$query = 'INSERT INTO auditLog (Event, Severity, IP, Time, Username, Password, SecQuestion, SecAnswer) VALUES (:event, :severity, :ip, NOW(), :user, :pass, :question, :answer);';
+	$dbquery = $myDBconnection -> prepare($query);
+	$dbquery -> bindValue(':event', $event);
+	$dbquery -> bindValue(':severity', $severity);
+	$dbquery -> bindValue(':ip', $IP);
+	$dbquery -> bindValue(':user', $username); 
+	$dbquery -> bindValue(':pass', $password);
+	$dbquery -> bindValue(':question', $secquestion);
+	$dbquery -> bindValue(':answer', $secanswer);
+	$dbquery -> execute();
+} catch (PDOException $e) {
+	$error_message = $e->getMessage();
+	echo "<p>An error occurred while trying to log data to the table: $error_message </p>";
+}
 }
 ?>
