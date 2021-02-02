63d396ac584488fb871a8cb2d57ae71faa5eff8e
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-02-01 23:32:16 -0500

    Add files via upload

--- register.php
+++ register.php
@@ -93,7 +93,26 @@ register.php
 							$dbquery -> bindValue(':answer', $sans);
 							$dbquery -> execute();
 							echo "You have been successfully Registered! Please try logging in.";
-							include 'logging.php';
+							// include 'logging.php';
+							function auditlog($event, $severity, $username, $password, $secquestion, $secanswer) {
+							$IP = $_SERVER['REMOTE_ADDR'];
+								try {
+									//Username, Password, Security Question, and Security Answer can be NULL in case they aren't used in certain events or are left blank
+									$query = 'INSERT INTO auditLog (Event, Severity, IP, Time, Username, Password, SecQuestion, SecAnswer) VALUES (:event, :severity, :ip, NOW(), :user, :pass, :question, :answer);';
+									$dbquery = $myDBconnection -> prepare($query);
+									$dbquery -> bindValue(':event', $event);
+									$dbquery -> bindValue(':severity', $severity);
+									$dbquery -> bindValue(':ip', $IP);
+									$dbquery -> bindValue(':user', $username); 
+									$dbquery -> bindValue(':pass', $password);
+									$dbquery -> bindValue(':question', $secquestion);
+									$dbquery -> bindValue(':answer', $secanswer);
+									$dbquery -> execute();
+								} catch (PDOException $e) {
+									$error_message = $e->getMessage();
+									echo "<p>An error occurred while trying to log data to the table: $error_message </p>";
+								}
+							}
 							auditlog('New Account Registered', 0, $suser, $spass, $squest, $sans);
 						}
 					} catch (PDOException $e) {
