3c3144c7c81870f65c70b4ba8443bc169ae34443
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-02-28 19:02:12 -0500

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,15 @@
+<!--
+Christopher Burgess
+2/28/2021
+nav.php
+-->
+<?php require "database.php" ?>
+<?php
+	if (isset($_SESSION['Admin'])) {
+		echo '<ul><li><a href="index.php">Home</a></li><li><a href="logout.php">Logout</a></li><li><a href="admin.php">Admin</a></li></ul>';
+	} elseif (isset($_SESSION['Username'])) {
+		echo '<ul><li><a href="index.php">Home</a></li><li><a href="logout.php">Logout</a></li></ul>';
+	} else {
+		echo '<ul><li><a href="index.php">Home</a></li><li><a href="login.php">Login</a></li><li><a href="register.php">Register</a></li></ul>';
+	}
+?>
