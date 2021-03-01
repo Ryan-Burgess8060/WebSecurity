5374796c2d97f63ec2c5616cc42ade9406a7b24a
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-02-28 23:18:59 -0500

    Add files via upload

--- cookie.php
+++ cookie.php
@@ -9,21 +9,21 @@ $loggedIn = False;
 $Admin = False;
 $token = 0;
 if(isset($_COOKIE['Authentication'])) {
-	// try {
+	try {
 		// $token = $_COOKIE['Authentication'];
-		// $query = 'SELECT * FROM sessions WHERE Token = :token AND Expiration > NOW();';
-		// $dbquery = $myDBconnection -> prepare($query);
-		// $dbquery -> bindValue(':token', $token); 
-		// $dbquery -> execute();
-		// $result = $dbquery -> fetch();
-		// if($result != "") {
-			// $loggedIn = True;
-			// if($result['Admin'] == "Yes") {
-				// $Admin = True;
-			// }
-		// }
-	// } catch {
-		// $error_message = $e->getMessage();
-	// }
+		$query = 'SELECT * FROM sessions WHERE Token = :token AND Expiration > NOW();';
+		$dbquery = $myDBconnection -> prepare($query);
+		$dbquery -> bindValue(':token', $token); 
+		$dbquery -> execute();
+		$result = $dbquery -> fetch();
+		if($result != "") {
+			$loggedIn = True;
+			if($result['Admin'] == "Yes") {
+				$Admin = True;
+			}
+		}
+	} catch {
+		$error_message = $e->getMessage();
+	}
 }
 ?>
