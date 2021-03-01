0191320005c091d57b4863b074480a0930cd7626
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-02-28 23:09:28 -0500

    Add files via upload

--- cookie.php
+++ cookie.php
@@ -5,25 +5,25 @@ cookie.php
 -->
 <?php 
 require "database.php";
-$loggedIn = False;
-$Admin = False;
-$token = 0;
-// if(isset($_COOKIE['Authentication'])) {
-	// try {
-		// $token = $_COOKIE['Authentication'];
-		// $query = 'SELECT * FROM sessions WHERE Token = :token AND Expiration > NOW();';
-		// $dbquery = $myDBconnection -> prepare($query);
-		// $dbquery -> bindValue(':token', $token); 
-		// $dbquery -> execute();
-		// $result = $dbquery -> fetch();
-		// if(!$result == "") {
-			// $loggedIn = True;
-			// if($result['Admin'] == "Yes") {
-				// $Admin = True;
-			// }
-		// }
-	// } catch {
-		// $error_message = $e->getMessage();
-	// }
-// }
+// $loggedIn = False;
+// $Admin = False;
+// $token = 0;
+if(isset($_COOKIE['Authentication'])) {
+	try {
+		$token = $_COOKIE['Authentication'];
+		$query = 'SELECT * FROM sessions WHERE Token = :token AND Expiration > NOW();';
+		$dbquery = $myDBconnection -> prepare($query);
+		$dbquery -> bindValue(':token', $token); 
+		$dbquery -> execute();
+		$result = $dbquery -> fetch();
+		if(!$result == "") {
+			$loggedIn = True;
+			if($result['Admin'] == "Yes") {
+				$Admin = True;
+			}
+		}
+	} catch {
+		$error_message = $e->getMessage();
+	}
+}
 ?>
