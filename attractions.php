8c4ced0c1b571b9a9190a325434b26a4d77c370d
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-04-27 21:04:56 -0400

    Add files via upload

---
+++ attractions.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Sleeping Eagles B &amp; B</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<main>
+		<article>
+			<form action="attractions.php" enctype="multipart/form-data" method="post">
+				<label for="newImage">Upload an attraction image:</label>
+				<input type="file" id="newImage" name="image">
+				<input type="submit" value="Upload an image" id="sub" name="mit">
+			</form>
+			<?php
+			if(isset($_POST['mit']))
+			{ 
+				$file = "images/" . $_FILES["image"]["name"];
+
+				if(move_uploaded_file($_FILES["image"]["tmp_name"], $file)) 
+				{
+					echo "<img src=".$file."  />";
+				} 
+				else 
+					{
+					echo "Error !!";
+					}
+			} 
+			?>
+			<br class="clear">
+		</article>
+	</main>
+</body>
+</html>
